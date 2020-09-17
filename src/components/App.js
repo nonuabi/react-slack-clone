a868d014ac0e9a0bbd1340fc473285205af3a461
nonuabi <nonuabi@gmail.com>
2020-09-17 20:33:40 +0530

    Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { SignIn } from './';
 
+function Home() {
+  return <div>Home</div>;
+}
+
+function Some() {
+  return <div>Some</div>;
+}
+
 class App extends Component {
-  state = {
-    counter : 1,
-  };
   render() {
-    return <div>
-      <SignIn />
-    </div>;
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/some" component={Some} />
+        </Switch>
+      </div>
+    );
   }
 }
 
