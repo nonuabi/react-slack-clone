dc3a3eccb7958149b737034dce0c6731c193b35a
nonuabi <nonuabi@gmail.com>
2020-09-20 18:01:45 +0530

    User social  auth Completed

--- src/firebase.js
+++ src/firebase.js
@@ -14,8 +14,53 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
 export const signInWithGoogle = () => {
-    const googleProvider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(googleProvider);
-} 
+  //Initialize google provider
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+  //Ask user to select gmail account in a new window.
+  auth.signInWithPopup(googleProvider);
+};
+
+export const signOut = () => {
+  auth.signOut();
+};
+
+export const createOrGetUserProfileDocument = async (user) => {
+  if (!user) return;
+
+  const userRef = firestore.doc(`users/${user.uid}`);
+  const snapshop = await userRef.get();
+
+  if (!snapshop.exists) {
+    const { displayName, email, photoURL } = user;
+
+    const createdAt = new Date();
+
+    try {
+      const user = {
+        display_name: displayName,
+        email,
+        photo_url: photoURL,
+      };
+      await userRef.set(user);
+    } catch (error) {
+      console.log('Error', error.message);
+    }
+  }
+
+  return getUserDocument(user.uid);
+};
+
+async function getUserDocument(uid) {
+  if (!uid) return null;
+
+  try {
+    const userDocument = await firestore.collection('user').doc(uid);
+    return userDocument;
+  } catch (error) {
+    console.error('Error in getUserDocument', error.message);
+  }
+}
